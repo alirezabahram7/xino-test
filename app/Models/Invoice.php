7fd8086210a94bba2bbfa3cef1e587746df0d349
alireza bahram <alib327@gmail.com>
2024-10-05 16:46:51 +0330

    feat: add invoice model

---
+++ app/Models/Invoice.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Invoice extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['user_id', 'amount', 'status', 'status_changed_at'];
+}
