671f3f5110cd7aec81fa4cd240a15feeca4418b2
alireza bahram <alib327@gmail.com>
2024-10-07 16:24:33 +0330

    chore: install api

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/webhook/users/subscription-renewal', [UserSubscriptionController::class, 'renew']);
 
-Route::middleware('auth:sanctum')->group(function () {
+//todo: add routes for login and logout
+//Route::middleware('auth:sanctum')->group(function () {
     Route::post('/users/subscribe', [UserSubscriptionController::class, 'subscribe']);
     Route::get('/users/invoices', [UserInvoiceController::class, 'index']);
     Route::get('/users/sections', [UserSectionController::class, 'index']);
-});
-
-//todo: add routes to CRUD other resources
+//});
